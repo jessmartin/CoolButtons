47cdde3906a13ef2c9e69ac55b501ac3b19d053e
Jess Martin <jessmartin@gmail.com>
2011-04-14 23:20:32 -0400

    Initial Commit

---
+++ CoolButtons/CoolButtonsAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  CoolButtonsAppDelegate.h
+//  CoolButtons
+//
+//  Created by Jess Martin on 4/14/11.
+//  Copyright 2011 Relevance, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoolButtonsAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ CoolButtons/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CoolButtons
+//
+//  Created by Jess Martin on 4/14/11.
+//  Copyright 2011 Relevance, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
